c274067448ff402fbf866d2b76596ddbd53b55b2
a <145933200+ivasosic@users.noreply.github.com>
2024-09-23 12:25:34 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace checkbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Dorucak = new System.Windows.Forms.CheckBox();
+            this.Rucak = new System.Windows.Forms.CheckBox();
+            this.Vecera = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Sakriji = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // Dorucak
+            // 
+            this.Dorucak.AutoSize = true;
+            this.Dorucak.Location = new System.Drawing.Point(243, 138);
+            this.Dorucak.Name = "Dorucak";
+            this.Dorucak.Size = new System.Drawing.Size(67, 17);
+            this.Dorucak.TabIndex = 0;
+            this.Dorucak.Text = "Dorucak";
+            this.Dorucak.UseVisualStyleBackColor = true;
+            this.Dorucak.CheckedChanged += new System.EventHandler(this.Dorucak_CheckedChanged);
+            // 
+            // Rucak
+            // 
+            this.Rucak.AutoSize = true;
+            this.Rucak.Location = new System.Drawing.Point(243, 162);
+            this.Rucak.Name = "Rucak";
+            this.Rucak.Size = new System.Drawing.Size(58, 17);
+            this.Rucak.TabIndex = 1;
+            this.Rucak.Text = "Rucak";
+            this.Rucak.UseVisualStyleBackColor = true;
+            this.Rucak.CheckedChanged += new System.EventHandler(this.Rucak_CheckedChanged);
+            // 
+            // Vecera
+            // 
+            this.Vecera.AutoSize = true;
+            this.Vecera.Location = new System.Drawing.Point(243, 185);
+            this.Vecera.Name = "Vecera";
+            this.Vecera.Size = new System.Drawing.Size(60, 17);
+            this.Vecera.TabIndex = 2;
+            this.Vecera.Text = "Vecera";
+            this.Vecera.UseVisualStyleBackColor = true;
+            this.Vecera.CheckedChanged += new System.EventHandler(this.Vecera_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(243, 231);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Posalji";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(380, 114);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(201, 140);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Sakriji
+            // 
+            this.Sakriji.AutoSize = true;
+            this.Sakriji.Location = new System.Drawing.Point(380, 287);
+            this.Sakriji.Name = "Sakriji";
+            this.Sakriji.Size = new System.Drawing.Size(99, 17);
+            this.Sakriji.TabIndex = 5;
+            this.Sakriji.Text = "Sakrij narudzbu";
+            this.Sakriji.UseVisualStyleBackColor = true;
+            this.Sakriji.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Sakriji);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Vecera);
+            this.Controls.Add(this.Rucak);
+            this.Controls.Add(this.Dorucak);
+            this.Name = "Form1";
+            this.Text = "Upotreba checkbox kontrole";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox Dorucak;
+        private System.Windows.Forms.CheckBox Rucak;
+        private System.Windows.Forms.CheckBox Vecera;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox Sakriji;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace checkbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Sakriji.Checked)
+            {
+                textBox1.Hide();
+            }
+            else
+            {
+                textBox1.Show();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Dorucak_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Dorucak.Checked)
+            {
+                textBox1.AppendText("Dorucak");
+            }
+        }
+
+        private void Rucak_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Rucak.Checked)
+            {
+                textBox1.AppendText("\nRucak");
+            }
+        }
+
+        private void Vecera_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Vecera.Checked)
+            {
+                textBox1.AppendText("\nVecera");
+            }
+        }
+    }
+}
+
